e7bc1b3d64ed49545a1819cc6c4071645b9185c1
Dankerowl420 <dunklehass14@gmail.com>
2026-02-16 13:42:01 -0500

    Portfolio upgraded to dynamic gallery

---
+++ js/gallery-data.js
@@ -0,0 +1,65 @@
+// =========================
+// GALLERY IMAGE LIST
+// Josh — only edit THIS file when adding photos
+// =========================
+
+const GALLERY = {
+
+  wood: [
+    "wood_1.jpg",
+    "wood_2.jpg",
+    "wood_3.jpg",
+    "wood_4.jpg",
+    "wood_5.jpg",
+    "wood_6.jpg",
+    "wood_7.jpg",
+    "wood_8.jpg",
+    "wood_9.jpg",
+    "wood_10.jpg",
+    "wood_11.jpg",
+    "wood_12.jpg"
+  ],
+
+  tumblers: [
+    "tumblers_1.jpg",
+    "tumblers_2.jpg",
+    "tumblers_3.jpg",
+    "tumblers_4.jpg",
+    "tumblers_5.jpg",
+    "tumblers_6.jpg",
+    "tumblers_7.jpg",
+    "tumblers_8.jpg"
+  ],
+
+  prints: [
+    "3dprints_1.jpg",
+    "3dprints_2.jpg",
+    "3dprints_3.jpg",
+    "3dprints_4.jpg",
+    "3dprints_5.jpg",
+    "3dprints_6.jpg"
+  ],
+
+  metal: [
+    "metal_1.jpg",
+    "metal_2.jpg",
+    "metal_3.jpg",
+    "metal_4.jpg",
+    "metal_5.jpg",
+    "metal_6.jpg"
+  ],
+
+  door: [
+    "door_hanger_1.jpg",
+    "door_hanger_2.jpg",
+    "door_hanger_3.jpg"
+  ],
+
+  custom: [
+    "custom_1.jpg",
+    "custom_2.jpg",
+    "custom_3.jpg",
+    "custom_4.jpg"
+  ]
+
+};
